a0bef67e552fc02c933561560b04923afd8751d1
rozmar <neurozmar@gmail.com>
2026-06-16 15:34:46 +0200

    touch epochs defined

--- ndnf_pipeline/behavior_analysis.py
+++ ndnf_pipeline/behavior_analysis.py
@@ -1,10 +1,12 @@
 import datajoint as dj
 import numpy as np
+import pandas as pd
 from scipy.optimize import curve_fit
 from ndnf_pipeline.utils.pipeline_tools import get_schema_name
 from ndnf_pipeline import experiment
 
 schema = dj.schema(get_schema_name('behavior_analysis'), locals())
+
 
 
 @schema
@@ -90,3 +92,106 @@ class BlockStatistics(dj.Computed):
                                     'r_squared': r_squared})
             except RuntimeError:
                 pass
+
+
+@schema
+class TouchDetectionParams(dj.Lookup):
+    definition = """
+    touch_param_id: smallint
+    ---
+    smooth_window_time: float  # (s) boxcar smoothing window applied to each force axis
+    sd_window_time: float      # (s) rolling SD window applied to force magnitude
+    sd_threshold: float        # (g) SD threshold above which contact is detected
+    """
+    contents = [
+        {'touch_param_id': 0, 'smooth_window_time': 0.3, 'sd_window_time': 0.05, 'sd_threshold': 0.05}
+    ]
+
+
+@schema
+class TrialTouchTimes(dj.Computed):
+    definition = """
+    -> experiment.BehaviorTrial
+    -> TouchDetectionParams
+    ---
+    n_touch_epochs: smallint  # number of contiguous touch epochs detected
+    """
+
+    @property
+    def key_source(self):
+        return experiment.BehaviorTrial * TouchDetectionParams & experiment.TrialForceTrace
+
+    class TouchEpoch(dj.Part):
+        definition = """
+        -> master
+        is_touch: bool       # True = touch, False = no touch
+        epoch_idx: smallint  # index within touch or no-touch epochs separately
+        ---
+        start_idx: int       # start sample index into the smoothed trace (inclusive)
+        end_idx: int         # end sample index into the smoothed trace (inclusive)
+        start_time: float    # (s) from trial start
+        end_time: float      # (s) from trial start
+        duration: float      # (s)
+        """
+
+    def make(self, key):
+        params = (TouchDetectionParams & key).fetch1()
+
+        force_trace_time = (experiment.TrialForceTrace & key).fetch1('force_trace_time')
+        if len(force_trace_time)==0:
+            print('empty trial')
+            return
+        sample_interval = float(np.median(np.diff(force_trace_time)))
+
+        smooth_window = max(1, int(params['smooth_window_time'] / sample_interval))
+        sd_window = max(2, int(params['sd_window_time'] / sample_interval))
+
+        axis_indices, axis_values = (experiment.TrialForceTrace.TrialForceAxis & key).fetch(
+            'force_axis_idx', 'force_trace_value', order_by='force_axis_idx'
+        )
+        force_axes = {int(idx): val for idx, val in zip(axis_indices, axis_values)}
+
+        smoothed = {
+            ax: np.convolve(force_axes[ax], np.ones(smooth_window) / smooth_window, mode='valid')
+            for ax in force_axes
+        }
+        n_smooth = min(len(v) for v in smoothed.values())
+        force_magnitude_smooth = np.sqrt(sum(smoothed[ax][:n_smooth] ** 2 for ax in smoothed))
+
+        force_magnitude_smooth_sd = pd.Series(force_magnitude_smooth).rolling(window=sd_window).std()
+        contact_times = (force_magnitude_smooth_sd > params['sd_threshold']).fillna(False).values
+
+        # Time array centered on each smoothing window
+        half_w = smooth_window // 2
+        smooth_time = force_trace_time[half_w: half_w + n_smooth]
+        if len(smooth_time) < n_smooth:
+            smooth_time = force_trace_time[0] + np.arange(n_smooth) * sample_interval
+
+        if len(contact_times) == 0:
+            self.insert1({**key, 'n_touch_epochs': 0})
+            return
+
+        changes = np.diff(contact_times.astype(int))
+        block_starts = np.concatenate([[0], np.where(changes != 0)[0] + 1])
+        block_ends = np.concatenate([np.where(changes != 0)[0], [len(contact_times) - 1]])
+
+        epochs = []
+        epoch_counters = {False: 0, True: 0}
+
+        for start, end in zip(block_starts, block_ends):
+            is_touch = bool(contact_times[start])
+            duration = float((end - start + 1) * sample_interval)
+            epochs.append({
+                **key,
+                'is_touch': is_touch,
+                'epoch_idx': epoch_counters[is_touch],
+                'start_idx': int(start),
+                'end_idx': int(end),
+                'start_time': float(smooth_time[start]),
+                'end_time': float(smooth_time[end]),
+                'duration': duration,
+            })
+            epoch_counters[is_touch] += 1
+
+        self.insert1({**key, 'n_touch_epochs': epoch_counters[True]})
+        self.TouchEpoch.insert(epochs)
